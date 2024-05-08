dc0b489c85403e984eec61041a183528e3a96669
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-05-08 16:42:20 -0400

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Card from './components/Card';
 
 function App() {
   return (
-    <div>
+    <div class="container m-auto">
       <header>
         <h1>Ninja Merch</h1>
       </header>
 
-      <img src={banner} alt="site banner" />
+      <img class="rounded-md" src={banner} alt="site banner" />
+      
+      <div class="grid grid-cols-4 gap-10 my-4">
+        <Card />
+        <Card />
+        <Card />
+      </div>
 
-      <Card />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 export default function Card() {
     return (
-        <div>
+        <div class="bg-white p-4 text-center rounded-md shadow-md">
             <h2>Card Component</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dicta non facere velit eaque exercitationem.</p>
+            <button class="btn">click me!</button>
         </div>
     )
 }
