d832021e56f76c458190d160338ff0bd852ae935
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-05-08 14:25:32 -0400

    remove logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
