16d681a47d4455df7c100f6b84dca31fc8c0358f
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-05-09 10:47:22 -0400

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { createSignal } from 'solid-js';
+import { Routes, Route, A } from "@solidjs/router";
 
 import banner from './assets/banner.png';
-
-import { Router, Route } from "@solidjs/router";
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 
@@ -16,24 +15,25 @@ function App() {
   return (
     <div class="container m-auto bg">
       <header
-        class="my-4 p-2 text-xl flex items-center gap-4"
+        class="my-4 p-2 text-xl flex items-center gap-4 justify-end"
         classList={{"bg-neutral-900": darkTheme(), "text-white": darkTheme() }}
       >
         <span 
           class="material-symbols-outlined cursor-pointer"
           onClick={toggleTheme}
         >light_mode</span>
-        <h1>Ninja Merch</h1>
+        <h1 class="mr-auto">Ninja Merch</h1>
+
+        <A href="/">Home</A>
+        <A href="/cart">Cart</A>
       </header>
 
       <img class="rounded-md" src={banner} alt="site banner" />
 
-      <Router>
+      <Routes>
         <Route path="/" component={Home} />
         <Route path="/cart" component={Cart} />
-      </Router>
-      
-      
+      </Routes>
 
     </div>
   );
